4c99314c3d9dd911c770cdea3f98a779c96768c6
floor777 <bvfwra@gmail.com>
2023-06-05 16:00:21 -0700

    Add navbar to homepage.jsx and include styling from homepage.css

--- client/src/homepage.jsx
+++ client/src/homepage.jsx
@@ -3,16 +3,50 @@ import './login.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import './homepage.css';
 
 function Homepage() { 
 
 
   return (
     <>
-    <div>
-        <h1>Homepage</h1>
-        <nav></nav>
-    </div>
+
+      <nav className='navbar'>
+      {/* <img src='./src/assets/react.svg'></img> */}
+      <a href="/"><img class="navbar-brand" src="./src/assets/react.svg" alt="HomePage"></img></a>
+
+      
+      {/* <h1 id='website-title'>Childfree Socially </h1> */}
+      <Link to="/">
+        <button id='homepage-title' type="button">
+          Childfree Socially
+        </button>
+      </Link>
+
+
+      <div id='signlog'>
+      <Link to="/login">
+        <button id='login-button' type="button">
+          Login
+        </button>
+      </Link>
+
+      <Link to="/signup">
+        <button id='signup-button' type="button">
+          Signup
+        </button>
+      </Link>
+
+
+        
+      </div>
+
+      </nav>
+     
+      {/* <h1>Homepage</h1> */}
+        
+
 
     </>
   )
