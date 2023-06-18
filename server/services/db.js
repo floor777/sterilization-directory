4dc5fcf3f7909171fb431f66fdfd6b763e336a54
floor777 <bvfwra@gmail.com>
2023-06-17 17:52:35 -0700

    Authenticate database in db.js config file instead of in index.js

--- server/services/db.js
+++ server/services/db.js
@@ -1,14 +1,25 @@
 const Sequelize = require("sequelize");
 require('dotenv').config()
+const mysql2 = require('mysql2');
+const cls = require('cls-hooked');
+const namespace = cls.createNamespace('CFS');
+Sequelize.useCLS(namespace);
+
 const sequelize = new Sequelize(
     process.env.DATABASE_NAME,
     process.env.DATABASE_USERNAME,
     process.env.DATABASE_PASSWORD,
     {
         host: process.env.DATABASE_HOSTNAME,
-        dialect: 'mysql'
+        dialect: 'mysql',
     }
-    );
+);
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+  });
+
 
 module.exports = {
     sequelize: sequelize
