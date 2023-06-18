0a3c39f2bfab4a32dc2d46b8b20b494838f2dd2b
floor777 <bvfwra@gmail.com>
2023-06-17 17:49:00 -0700

    Set up vite.config.js for docker to host 5173 port externally

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    protocol: 'ws',
+    host: true,
+    strictPort: true,
+    port: 5173
+  }
 })
