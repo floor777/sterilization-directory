da766f00b3f37be7c5c62247b369590526e50ed9
floor777 <bvfwra@gmail.com>
2023-06-23 14:39:51 -0700

    Add conditional statements for different node environments

--- server/services/db.js
+++ server/services/db.js
@@ -4,16 +4,52 @@ const mysql2 = require('mysql2');
 const cls = require('cls-hooked');
 const namespace = cls.createNamespace('CFS');
 Sequelize.useCLS(namespace);
+let sequelize;
+require('dotenv').config()
 
-const sequelize = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_USERNAME,
-    process.env.DATABASE_PASSWORD,
+
+if(process.env.NODE_ENV === "dev") {
+  console.log('Currenly in dev environment');
+  sequelize = new Sequelize(
+    process.env.DATABASE_NAME_DEV,
+    process.env.DATABASE_USERNAME_DEV,
+    process.env.DATABASE_PASSWORD_DEV,
     {
-        host: process.env.DATABASE_HOSTNAME,
-        dialect: 'mysql',
+      host: process.env.DATABASE_HOSTNAME_DEV,
+      dialect: 'mysql',
     }
 );
+
+}
+else if(process.env.NODE_ENV ==="test") {
+  console.log("Currenly in test environment");
+  sequelize = new Sequelize(
+    process.env.DATABASE_NAME_TEST,
+    process.env.DATABASE_USERNAME_TEST,
+    process.env.DATABASE_PASSWORD_TEST,
+    {
+      host: process.env.DATABASE_HOSTNAME_TEST,
+      dialect: 'mysql',
+    }
+);
+}
+
+else if(process.env.NODE_ENV === "prod") {
+  console.log('Currenly in prod environment');
+
+  sequelize = new Sequelize(
+    process.env.DATABASE_NAME_PROD,
+    process.env.DATABASE_USERNAME_PROD,
+    process.env.DATABASE_PASSWORD_PROD,
+    {
+      host: process.env.DATABASE_HOSTNAME_PROD,
+      dialect: 'mysql',
+    }
+);
+
+}
+
+
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
   }).catch((error) => {
