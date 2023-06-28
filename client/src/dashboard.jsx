3cc394b00a69b1afd2ef8eea7178137db3be96ad
floor777 <bvfwra@gmail.com>
2023-06-27 21:45:51 -0700

    Remove unnecessary login states and related functions and add import of google map api map

--- client/src/dashboard.jsx
+++ client/src/dashboard.jsx
@@ -4,12 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import Home from './map';
 
 
 function Dashboard() { 
     const [authorizedUsername, setAuthorizedUserName] = useState("");
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
     const navigate = useNavigate();
 
     const logout = () => {
@@ -21,45 +20,21 @@ function Dashboard() {
       })
       .then(response => {
         if(response.config.method === "post" && response.data == "OK" ) {
-          console.log(response);
+          // console.log(response);
           console.log('Logout was successful');
           navigate("/");
           
         }
         else  {
-          console.log(response.config.method);
-          console.log(response.status);
-          console.log(response.data);
+          // console.log(response.config.method);
+          // console.log(response.status);
+          // console.log(response.data);
           console.log('Non-200 response recorded in dashboard.jsx');
         }
       }).catch(err => {
         console.log(err);
       });
     }
-    const login = () => {
-
-      axios({
-        method: "post",
-        data: {
-          email: loginEmail,
-          password: loginPassword,
-        },
-        withCredentials: true,
-        url: "http://localhost:3000/login"
-              
-      })
-      .then(response => {
-        if(response.status === 200) {
-          console.log('Login was successful');
-          navigate("/dashboard");
-        }
-        else  {
-          console.log('Non-200 response recorded in dashboard.jsx');
-        }
-      }).catch(err => {
-        console.log(err);
-      });
-    };
 
     const getUser = () => {
       axios({
@@ -79,9 +54,14 @@ function Dashboard() {
     <div>
         <h1>Welcome back, {authorizedUsername}!</h1>
         <p>Find a nearby doctor who understands and respects the decisions you make</p>
+        
+
 
         <button onClick={getUser}>Get loggedin user</button>
         <button onClick={logout}>Logout</button>
+        <Home />
+        
+        
 
 
     </div>
