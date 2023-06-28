85914d065e82ff31bc5ecd63ac0bc5e1bffd2e70
floor777 <bvfwra@gmail.com>
2023-06-27 22:54:30 -0700

    Add initial model for markers

---
+++ server/models/marker.model.js
@@ -0,0 +1,37 @@
+const {Sequelize, DataTypes} = require("sequelize");
+const db = require('../services/db.js');
+
+const Marker = db.sequelize.define('markers', {
+    id: {
+        type: DataTypes.INTEGER,
+        initialAutoIncrement: 1,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    lat: {
+        type: DataTypes.DECIMAL(10,6),
+        allowNull: false
+    },
+    lng: {
+        type: DataTypes.DECIMAL(10,6),
+        allowNull: false
+
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+
+    
+});
+
+db.sequelize.sync().then(() => {
+    console.log('Markers table created successfully');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+
+module.exports = {
+    Marker: Marker
+  }
