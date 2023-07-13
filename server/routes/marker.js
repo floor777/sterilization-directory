12bce6397a9679c45dbdba4c42c8ba289fc330d7
floor777 <bvfwra@gmail.com>
2023-07-12 17:14:53 -0700

    Remove unnecessary console.log statements and unused route

--- server/routes/marker.js
+++ server/routes/marker.js
@@ -3,48 +3,62 @@ const { Marker } = require('../models/marker.model');
 const db = require('../services/db');
 const router = express.Router();
 
-
-
 // Get all markers in the markers table
 router.get('/all', async (req, res) => {
     const markers = await Marker.findAll();
-    console.log(markers.every(marker => marker instanceof Marker)); // true
-    console.log('All markers:', JSON.stringify(markers, null, 2));  
     res.send(markers);
-    
-
 });
+// Check if a marker exists in the database already
+router.get('/exists', async( req, res) => {
+  let fixedLat = Number(req.query.lat).toFixed(6);
+  let fixedLng = Number(req.query.lng).toFixed(6);
 
-router.post('/', async (req, res) => {
-    db.sequelize.transaction(async () => {
-        
-        const searchedMarker = await Marker.findOne({
-          where: {
-            lat: req.query.lat,
-            lng: req.query.lng
-          },
-          
-    
-        });
-    
-        if(searchedMarker === null) {
-          res.status(200).send({ 
-            message: 'Marker was created',
+  const marker = await Marker.findOne({
+    where: {
+      lat: fixedLat,
+      lng: fixedLng
+    },
 
-            lat: req.query.lat,
-            lng: req.query.lng,
-            title: req.query.title
-          });
-          return Marker.create({
-            lat: req.query.lat,
-            lng: req.query.lng,
-            title: req.query.title
-          })
-        }
-        else {
-          res.status(400).send({ message: 'Marker already exists at that position' });
-        }
+  });
+
+  if(marker === null) {
+
+
+    res.status(200).send({ 
+      message: 'Marker at this lat/lng does not exist yet'
+    });
+  
+
+  }
+  else {
+
+    res.status(200).send({
+      message: 'Marker at this lat/lng already exists',
+      marker: marker
+    });
+ 
+  }
+})
+
+// Build a new marker and save it in teh database
+router.post('/build', async (req, res) => {
+  let fixedLat = Number(req.query.lat).toFixed(6);
+  let fixedLng = Number(req.query.lng).toFixed(6);
+
+  try {
+    let response = await Marker.create({
+      lat: fixedLat,
+      lng: fixedLng,
+      title: req.query.title
+    });
+
+    res.send({marker: response})
     
-      })
-});
+  } 
+  catch (error) {
+    console.log(error)
+    
+  }
+  
+})
 module.exports = router;
