ea4eb4d002f33571b47f4adc3f67ccea2306c2d3
floor777 <bvfwra@gmail.com>
2023-07-14 23:27:05 -0700

    Add protected route to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,26 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Login from './login';
 import Signup from './signup';
-import Homepage from './homepage';
-import Dashboard from './dashboard';
+import Homepage from './homepage.jsx';
+import Dashboard from './dashboard.jsx';
+import RequireAuth from './requireauth';
+
 
 function App() {
   return (
     
     <Routes>
+      <Route
+       path='/dashboard' element= {
+      <RequireAuth>
+        <Dashboard />
+
+      </RequireAuth>
+    }/>
       
       <Route path='/login' element={<Login />}></Route>
       <Route path='/signup' element={<Signup />}></Route>
       <Route exact path='/' element={<Homepage />}></Route>
-      <Route path='/dashboard' element={<Dashboard />}></Route>
       
     </Routes>
   )
