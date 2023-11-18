ddd9de9532a220553e7915f55e9697279444c5b7
floor777 <bvfwra@gmail.com>
2023-11-18 02:29:11 -0800

    Modificatio of config to check for env var validation

--- server/services/db.js
+++ server/services/db.js
@@ -10,7 +10,7 @@ const serverCa = [fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")];
 console.log('Initializing database');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 console.log("checkz")
-console.log(process.env.DATABASE_NAME === null);
+console.log(process.env.DATABASE_NAME.length);
   sequelize = new Sequelize(
     process.env.DATABASE_NAME,
     process.env.DATABASE_USERNAME,
